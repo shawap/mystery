8e06368d09bdf59a98a9478bc0d1e84af9a4cff5
Ian Tsai <ianpaul32@gmail.com>
2018-07-12 21:18:32 +0800

    fix bug && update
    wrong  for loop

--- testCcode/perceptNN.c
+++ testCcode/perceptNN.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 
 int layerCnt, INPUT_SIZE, OUTPUT_SIZE;
@@ -12,8 +13,10 @@ int main()
     scanf("%d%d%d", &layerCnt, &INPUT_SIZE, &OUTPUT_SIZE);
     
     FILE *fptr;
-    int i, j, k, maxValue;
+    int i, j, k, maxValue, predict_value[10005], cnt = 0;
+    int correct, wrong, correct_num;
     double input_value[INPUT_SIZE], output_weights[OUTPUT_SIZE][INPUT_SIZE], output_bias[OUTPUT_SIZE], predict[OUTPUT_SIZE];
+    double correct_percent;
     char c;
 
     printf("Total Layer is : \t%d\n", layerCnt);
@@ -22,23 +25,13 @@ int main()
 
 
 
-    /*  get intput layer weight   */
-    fptr = fopen("image_info.txt", "r");
-    if(fptr != NULL)
-        printf("\nOpen image_info success!\n");
-
-    for(i = 0 ; i < INPUT_SIZE ; i++)
-        fscanf(fptr, "%lf", &input_value[i]);
-    printf("Finish \"input value\" input\n");
-    fclose(fptr);
-
     /*  get output layer weights   */
     fptr = fopen("perceptWeight.txt", "r");
-    if(fptr != NULL)
-        printf("\nOpen perceptWeight success!\n");
+    assert(fptr != NULL );
+    printf("\nOpen perceptWeight success!\n");
 
-    for(i = 0 ; i < OUTPUT_SIZE ; i++)
-        for(j = 0 ; j < INPUT_SIZE ; j++)
+    for(j = 0 ; j < INPUT_SIZE ; j++)
+        for(i = 0 ; i < OUTPUT_SIZE ; i++)
             fscanf(fptr, "%lf", &output_weights[i][j]);
     printf("Finish \"output Weights\" input\n");
     fclose(fptr);
@@ -51,8 +44,8 @@ int main()
 
     /*  get output layer bias   */
     fptr = fopen("perceptBias.txt", "r");
-    if(fptr != NULL)
-        printf("\nOpen perceptBias success!\n");
+    assert(fptr != NULL );
+    printf("\nOpen perceptBias success!\n");
         
     for(i = 0 ; i < OUTPUT_SIZE ; i++)
         fscanf(fptr,"%lf", &output_bias[i]);
@@ -66,25 +59,67 @@ int main()
         printf("%lf\n", output_bias[i]);
     */
 
-    /*  compute   */
-    for(i = 0 ; i < OUTPUT_SIZE ; i++)
-    {
-        predict[i] = 0;
-        for(j = 0 ; j < INPUT_SIZE ; j++)
+
+
+    /*  get intput layer weight   */
+    fptr = fopen("test_images.txt", "r");
+    assert(fptr != NULL );
+    printf("\nOpen image_info success!\n");
+
+    cnt = 0;
+    while(!feof(fptr))
+    {    
+        for(i = 0 ; i < INPUT_SIZE ; i++)
+            fscanf(fptr, "%lf", &input_value[i]);
+        printf("Finish \"input value\" input\n");
+        
+        /*  compute   */
+        for(i = 0 ; i < OUTPUT_SIZE ; i++)
         {
-            predict[i] +=  output_weights[i][j] * input_value[j] ;
+            predict[i] = 0;
+            for(j = 0 ; j < INPUT_SIZE ; j++)
+            {
+                predict[i] +=  output_weights[i][j] * input_value[j] ;
+            }
+            predict[i] += output_bias[i];
         }
-        predict[i] += output_bias[i];
+
+
+        maxValue = 0;
+        for(i = 0 ; i < OUTPUT_SIZE ; i++)
+        {
+            if(predict[i] > predict[maxValue])
+                maxValue = i;
+            //printf("Label %d : %lf\n", i, predict[i]);
+        }
+        //printf("\nPredict is %d\n", maxValue);
+
+        predict_value[cnt++] = maxValue;
     }
+    fclose(fptr);
+
+    //for(i = 0 ; i < cnt ; i++)
+        //printf("%d\n", predict_value[i]);
+    
 
 
-    maxValue = 0;
-    for(i = 0 ; i < OUTPUT_SIZE ; i++)
+    fptr = fopen("test_label_list.txt", "r");
+    assert(fptr != NULL );
+
+    correct = wrong = 0;
+    for(i = 0 ; i < cnt ; i++)
     {
-        if(predict[i] > predict[maxValue])
-            maxValue = i;
-        printf("Label %d : %lf\n", i, predict[i]);
+        fscanf(fptr, "%d", &correct_num);
+        if(predict_value[i] == correct_num)
+            correct++;
+        else
+            wrong++;
     }
-    printf("\nPredict is %d\n", maxValue);
+    fclose(fptr);
+    correct_percent = (double) correct / (double ) (correct + wrong);
+    printf("correct percentage : %.2lf% , correct number is %d.\n", correct_percent, correct);
+
+
+
     return 0;
 }
